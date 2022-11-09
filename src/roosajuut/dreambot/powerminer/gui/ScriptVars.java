d7c6c6de82d8cc43a5fb16658362b6a1cbd77154
RoosaJuut <henri.vaniko@gmail.com>
2022-11-09 19:41:38 +0200

    v1.7

---
+++ src/roosajuut/dreambot/powerminer/gui/ScriptVars.java
@@ -0,0 +1,12 @@
+package roosajuut.dreambot.powerminer.gui;
+
+import roosajuut.dreambot.scriptmain.powerminer.MineTask;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ScriptVars {
+	public List<MineTask> tasks = new ArrayList<MineTask>();
+	public boolean started = false;
+}
